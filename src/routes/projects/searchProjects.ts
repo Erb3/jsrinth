7a4e4aa335a4f776e8a25f19a431b2d77d7d5f6f
Erlend <49862976+Erb3@users.noreply.github.com>
2023-12-13 21:02:36 +0100

    Did someone mention facet building v2

--- src/routes/projects/searchProjects.ts
+++ src/routes/projects/searchProjects.ts
@@ -1,34 +1,26 @@
 import { ModrinthAPI } from "../../ModrinthAPI";
 
-export interface SearchOptions {
-  query?: string;
-  facets?: Facet[] | Facet;
-  index?: "relevance" | "downloads" | "follows" | "newest" | "updated";
-  offset?: number;
-  limit?: number;
-}
-
 export async function searchProjects(
   parent: ModrinthAPI,
   options: SearchOptions
 ) {}
 
 export enum FacetType {
-  project_type,
-  categories,
-  versions,
-  client_side,
-  server_side,
-  open_source,
-  title,
-  author,
-  follows,
-  project_id,
-  license,
-  downloads,
-  color,
-  created_timestamp,
-  modified_timestamp,
+  project_type = "projectType",
+  categories = "categories",
+  versions = "versions",
+  client_side = "clientSide",
+  server_side = "serverSide",
+  open_source = "openSource",
+  title = "title",
+  author = "author",
+  follows = "follows",
+  project_id = "projectId",
+  license = "license",
+  downloads = "downloads",
+  color = "color",
+  created_timestamp = "createdTimestamp",
+  modified_timestamp = "modifiedTimestamp",
 }
 
 export enum FacetOperation {
@@ -42,40 +34,113 @@ export enum FacetOperation {
 
 export type FacetValue = string | number;
 
-export const Facets = {
-  equals(type: FacetType, val: FacetValue) {
-    return new Facet(type, FacetOperation["="], val);
-  },
-  notEquals(type: FacetType, val: FacetValue) {
-    return new Facet(type, FacetOperation["!="], val);
-  },
-  greaterThanOrEquals(type: FacetType, val: FacetValue) {
-    return new Facet(type, FacetOperation[">="], val);
-  },
-  greaterThan(type: FacetType, val: FacetValue) {
-    return new Facet(type, FacetOperation[">"], val);
-  },
-  lessThanOrEquals(type: FacetType, val: FacetValue) {
-    return new Facet(type, FacetOperation["<="], val);
-  },
-  lessThan(type: FacetType, val: FacetValue) {
-    return new Facet(type, FacetOperation["<"], val);
-  },
+export const Facets: { [key in FacetType]: () => SingleFacetBuilder } = {
+  [FacetType.project_type]: () =>
+    new SingleFacetBuilder(FacetType.project_type),
+  [FacetType.categories]: () => new SingleFacetBuilder(FacetType.categories),
+  [FacetType.versions]: () => new SingleFacetBuilder(FacetType.versions),
+  [FacetType.client_side]: () => new SingleFacetBuilder(FacetType.client_side),
+  [FacetType.server_side]: () => new SingleFacetBuilder(FacetType.server_side),
+  [FacetType.open_source]: () => new SingleFacetBuilder(FacetType.open_source),
+  [FacetType.title]: () => new SingleFacetBuilder(FacetType.title),
+  [FacetType.author]: () => new SingleFacetBuilder(FacetType.author),
+  [FacetType.follows]: () => new SingleFacetBuilder(FacetType.follows),
+  [FacetType.project_id]: () => new SingleFacetBuilder(FacetType.project_id),
+  [FacetType.license]: () => new SingleFacetBuilder(FacetType.license),
+  [FacetType.downloads]: () => new SingleFacetBuilder(FacetType.downloads),
+  [FacetType.color]: () => new SingleFacetBuilder(FacetType.color),
+  [FacetType.created_timestamp]: () =>
+    new SingleFacetBuilder(FacetType.created_timestamp),
+  [FacetType.modified_timestamp]: () =>
+    new SingleFacetBuilder(FacetType.modified_timestamp),
 };
 
-export class Facet {
-  facet: string[] = [];
+export interface SearchOptions {
+  query?: string;
+  facets?: SingleFacetBuilder[] | SingleFacetBuilder;
+  sort?: "relevance" | "downloads" | "follows" | "newest" | "updated";
+  offset?: number;
+  limit?: number;
+}
 
-  constructor(type: FacetType, op: FacetOperation, val: FacetValue) {
-    this.facet.push(
-      `${FacetType[type]} ${FacetOperation[op]} ${val.toString()}`
-    );
+class SingleFacetBuilder {
+  facetType: FacetType;
+  facetOperation?: FacetOperation;
+  facetValue?: FacetValue;
+  alternatives: string[] = [];
+
+  constructor(facetType: FacetType) {
+    this.facetType = facetType;
   }
 
-  or(...facets: Facet[]) {
-    facets.forEach((facet) => {
-      this.facet.push(...facet.facet);
-    });
+  equals(value: FacetValue): SingleFacetBuilder {
+    this.facetOperation = FacetOperation["="];
+    this.facetValue = value;
+    this.build();
     return this;
+  }
+
+  notEquals(value: FacetValue): SingleFacetBuilder {
+    this.facetOperation = FacetOperation["!="];
+    this.facetValue = value;
+    this.build();
+    return this;
+  }
+
+  greaterThanOrEqualsTo(value: FacetValue): SingleFacetBuilder {
+    this.facetOperation = FacetOperation[">="];
+    this.facetValue = value;
+    this.build();
+    return this;
+  }
+
+  greaterThan(value: FacetValue): SingleFacetBuilder {
+    this.facetOperation = FacetOperation[">"];
+    this.facetValue = value;
+    this.build();
+    return this;
+  }
+
+  lessThanOrEqualsTo(value: FacetValue): SingleFacetBuilder {
+    this.facetOperation = FacetOperation["<="];
+    this.facetValue = value;
+    this.build();
+    return this;
+  }
+
+  lessThan(value: FacetValue): SingleFacetBuilder {
+    this.facetOperation = FacetOperation["<"];
+    this.facetValue = value;
+    this.build();
+    return this;
+  }
+
+  or(...facets: SingleFacetBuilder[] | FacetValue[]) {
+    facets.forEach((facet) => {
+      if (facet instanceof SingleFacetBuilder) {
+        this.alternatives.push(...facet.alternatives);
+      } else {
+        const newFacet = new SingleFacetBuilder(this.facetType);
+        newFacet.facetOperation = this.facetOperation;
+        newFacet.facetValue = facet;
+        newFacet.build();
+        this.alternatives.push(...newFacet.alternatives);
+      }
+    });
+
+    return this;
+  }
+
+  build(): string[] {
+    if (this.facetOperation === undefined || this.facetValue === undefined)
+      throw new Error("Cannot build facet without operation and value.");
+
+    this.alternatives.push(
+      `${this.facetType} ${FacetOperation[this.facetOperation]} ${
+        this.facetValue
+      }`
+    );
+
+    return this.alternatives;
   }
 }
--- tests/routes/projects/search.test.ts
+++ tests/routes/projects/search.test.ts
@@ -1,21 +1,26 @@
 import { ModrinthAPI } from "../../../src/ModrinthAPI";
-import { FacetType, Facets } from "../../../src/routes/projects/searchProjects";
+import { Facets } from "../../../src/routes/projects/searchProjects";
 const api = new ModrinthAPI({
   userAgent: "JSRinth Unit Tests",
 });
 
 test("Facet building", () => {
-  expect(
-    [
-      Facets.greaterThanOrEquals(FacetType.follows, 100).or(
-        Facets.greaterThanOrEquals(FacetType.downloads, 2500)
-      ),
-      Facets.equals(FacetType.author, "Erb3").or(
-        Facets.equals(FacetType.author, "ModrinthArchives")
-      ),
-    ].map((v) => v.facet)
-  ).toEqual([
+  expect([
+    Facets.follows()
+      .greaterThanOrEqualsTo(100)
+      .or(Facets.downloads().greaterThanOrEqualsTo(2500)).alternatives,
+    Facets.author().equals("Erb3").or("ModrinthArchives", "Lemmmy")
+      .alternatives,
+  ]).toEqual([
     ["follows >= 100", "downloads >= 2500"],
-    ["author = Erb3", "author = ModrinthArchives"],
+    ["author = Erb3", "author = ModrinthArchives", "author = Lemmmy"],
   ]);
 });
+
+// test("Searching", () => {
+//   api.searchProjects({
+//     query: "Cactusfix",
+//     sort: "downloads",
+//     facets: [Facets.categories().equals("fabric")],
+//   });
+// });
